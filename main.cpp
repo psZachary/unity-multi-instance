12e54dae3db9942245623351d525f945cb605192
psZachary <47261750+psZachary@users.noreply.github.com>
2025-08-27 21:44:02 -0400

    Unity version checking support

--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 #include <Windows.h>
-#include <TlHelp32.h>
 #include <string>
-#include <comdef.h>
 #include <vector>
+#include <ShlObj.h>
+#include <propkey.h>
+#pragma comment(lib, "Version.lib")
+
+const wchar_t* supported_unity_versions[128] = {
+    L"2022.3.62f1",
+    L"2022.3.41f1"
+};
 
 static void print_usage() {
 	std::cout << "usage: unity-multi-instance.exe <image name>" << std::endl;
@@ -41,13 +47,13 @@ static bool get_image_section_info(HANDLE process, uintptr_t image_base, const c
     if (!rpm(process, nt_addr, &nt_leader, sizeof(nt_leader))) return 0;
     if (nt_leader.signature != IMAGE_NT_SIGNATURE) return 0;
 
-    const WORD numberOfSections = nt_leader.fileheader.NumberOfSections;
-    const WORD sizeOfOptional = nt_leader.fileheader.SizeOfOptionalHeader;
+    const WORD num_sections = nt_leader.fileheader.NumberOfSections;
+    const WORD size_of_optional = nt_leader.fileheader.SizeOfOptionalHeader;
 
     const uintptr_t sections_addr =
-        nt_addr + sizeof(DWORD) + sizeof(IMAGE_FILE_HEADER) + static_cast<uintptr_t>(sizeOfOptional);
+        nt_addr + sizeof(DWORD) + sizeof(IMAGE_FILE_HEADER) + static_cast<uintptr_t>(size_of_optional);
 
-    for (WORD i = 0; i < numberOfSections; ++i) {
+    for (WORD i = 0; i < num_sections; ++i) {
         IMAGE_SECTION_HEADER sh{};
         const uintptr_t sh_addr = sections_addr + static_cast<uintptr_t>(i) * sizeof(IMAGE_SECTION_HEADER);
         if (!rpm(process, sh_addr, &sh, sizeof(sh))) return 0; 
@@ -100,7 +106,7 @@ static uintptr_t sig_scan(HANDLE process, uintptr_t module_base, size_t size, co
     return 0; 
 }
 
-using t_on_image_loaded = void(*)(HANDLE h, uintptr_t);
+using t_on_image_loaded = bool(*)(HANDLE h, uintptr_t, wchar_t* image_path);
 
 static bool wait_for_module_load(PROCESS_INFORMATION pi, const wchar_t* mod_name, t_on_image_loaded on_load) {
     DEBUG_EVENT         dbg                         {};
@@ -115,22 +121,21 @@ static bool wait_for_module_load(PROCESS_INFORMATION pi, const wchar_t* mod_name
             auto& ev = dbg.u.LoadDll;
             uintptr_t base = reinterpret_cast<uintptr_t>(ev.lpBaseOfDll);
 
-            std::wstring module_name;
-
             if (ev.lpImageName && ev.fUnicode) {
                 LPVOID remote_buffer = nullptr;
 
-                if (rpm(pi.hProcess, (uintptr_t)ev.lpImageName, &remote_buffer, sizeof(remote_buffer)) && remote_buffer) {
-                    wchar_t buf[MAX_PATH] = {};
-
-                    if (rpm(pi.hProcess, (uintptr_t)remote_buffer, buf, sizeof(buf))) {
-                        module_name = buf;
-                    }
-                }
+                if (!rpm(pi.hProcess, (uintptr_t)ev.lpImageName, &remote_buffer, sizeof(remote_buffer)) || !remote_buffer)
+                    return false;
+                
+                if (!rpm(pi.hProcess, (uintptr_t)remote_buffer, module_name, sizeof(module_name)))
+                    return false;
+                
             }
 
-            if (module_name.find(mod_name) != std::string::npos) {
-                on_load(pi.hProcess, base);
+            if (wcsstr(module_name, mod_name) != 0) {
+                if (!on_load(pi.hProcess, base, module_name))
+                    return false;
+
                 found = true;
                 break;
             }
@@ -154,16 +159,135 @@ static bool wait_for_module_load(PROCESS_INFORMATION pi, const wchar_t* mod_name
 
     return true;
 }
+static bool get_product_version(const wchar_t* path, wchar_t** version) {
+    struct LANGANDCODEPAGE { WORD w_language; WORD w_codepage; };
+    LANGANDCODEPAGE* lp_translate = nullptr;
+    UINT cb_translate = 0;
+    uint8_t file_version_buffer[2048]{};
+    wchar_t blk_sub[50];
+    DWORD handle = 0;
+    DWORD size = 0;
+    wchar_t* value = nullptr;
+    UINT size_out = 0;
 
-static void on_unityplayer_load(HANDLE process, uintptr_t image_base) {
-    section_info        sinfo                   {};
-    uintptr_t           patch_instruction =     0;
+    if (!path || !version)
+        return false;
+
+    size = GetFileVersionInfoSizeW(path, &handle);
+    if (size == 0) 
+        return false;
+
+    if (!GetFileVersionInfoW(path, 0, size, file_version_buffer))
+        return false;    
+
+    if (!VerQueryValueW(file_version_buffer, L"\\VarFileInfo\\Translation",
+        (LPVOID*)&lp_translate, &cb_translate))
+        return L"";
+
+    swprintf_s(blk_sub, L"\\StringFileInfo\\%04x%04x\\ProductVersion",
+        lp_translate[0].w_language, lp_translate[0].w_codepage);
+    
+    if (VerQueryValueW(file_version_buffer, blk_sub, (LPVOID*)version, &size_out))
+    {
+        return true;
+    }
+
+    return false;
+}
+
+bool split_string(const wchar_t* wstr, wchar_t delimiter, wchar_t* out, size_t index) {
+    if (!wstr || !out) return false;
+
+    size_t cur = 0;
+    const wchar_t* p = wstr;
+
+    while (*p) {
+        while (*p == delimiter)
+            ++p;
+
+        if (!*p) break;
+
+        const wchar_t* start = p;
+        while (*p && *p != delimiter)
+            ++p; 
+
+        if (cur == index) {
+            size_t len = static_cast<size_t>(p - start);
+            wmemcpy(out, start, len);
+            out[len] = L'\0';
+            return true;
+        }
+        ++cur;
+    }
+
+    return false;
+}
+
+static bool unityplayer_version_supported(const wchar_t* image_path, bool* supported, wchar_t* version) {
+    wchar_t delimited_product_version[128]{};
+    wchar_t* product_version = nullptr;
+
+    if (!supported || !version || !image_path) return false;
+
+    if (!get_product_version(image_path, (wchar_t**)&product_version))
+    {
+        std::cout << "failed to get product version: " << GetLastError() << std::endl;
+        return false;
+    }
+
+    if (!split_string(product_version, ' ', delimited_product_version, 0)) {
+        std::cout << "failed to deliminate string: " << GetLastError() << std::endl;
+        return false;
+    }
+
+    if (!wmemcpy(version, product_version, wcslen(delimited_product_version)))
+    {
+        std::cout << "failed to copy product version wstring: " << GetLastError() << std::endl;
+        return false;
+    }
+
+    *supported = false;
+
+    int svi = 0;
+    do {
+        if (supported_unity_versions[svi]) {
+            if (_wcsicmp(version, supported_unity_versions[svi]) == 0)
+                *supported = true;
+        }
+    } while (supported_unity_versions[++svi]);
+
+
+    return true;
+}
+
+static bool on_unityplayer_load(HANDLE process, uintptr_t image_base, wchar_t* image_path) {
+    section_info sinfo {};
+    uintptr_t patch_instruction = 0;
+    wchar_t unity_version[128]{};
+    bool version_supported = false;
 
     std::cout << "loaded unityplayer: " << std::hex << image_base << std::endl;
 
+    if (!unityplayer_version_supported(image_path, &version_supported, unity_version)) {
+        std::cout << "failed to obtain unity support: " << GetLastError();
+        return false;
+    }
+
+    std::wcout << L"unity version: " << unity_version << std::endl;
+    if (!version_supported)
+    {
+        std::cout << "unsupported unity version detected, this will likely fail" << std::endl;
+        std::cout << "would you like to continue (y/n)?: ";
+        char choice = (char)std::cin.get();
+        if (choice == 'N' || choice == 'n')
+            return false;
+    }
+    else
+        std::cout << "unity version supported" << std::endl;
+
     if (!get_image_section_info(process, image_base, ".text", &sinfo)) {
         std::cout << "failed to get text section info: " << GetLastError() << std::endl;
-        return;
+        return false;
     }
 
     std::cout << "text section base: " << sinfo.base << std::endl;
@@ -172,7 +296,7 @@ static void on_unityplayer_load(HANDLE process, uintptr_t image_base) {
     patch_instruction = sig_scan(process, sinfo.base, sinfo.size, "48 8B DF 48 89 1D ?? ?? ?? ?? 40 38 7D ?? 75 ?? 44 8B 45 ?? 48 8B 55 ?? 48 8B 0D ?? ?? ?? ?? E8 ?? ?? ?? ?? 48 8B 1D ?? ?? ?? ?? 48 8B CB E8 ?? ?? ?? ?? 0F B6 F0 84 C0 74 ?? 48 8B CB E8 ?? ?? ?? ?? 84 C0 0F 84 ?? ?? ?? ?? 40 84 F6 0F 94 C3");
     if (!patch_instruction) {
         std::cout << "signature scan invalid: " << GetLastError() << std::endl;
-        return;
+        return false;
     }
     patch_instruction += 0x38;
     std::cout << "instruction: " << patch_instruction << std::endl;
@@ -193,6 +317,8 @@ static void on_unityplayer_load(HANDLE process, uintptr_t image_base) {
     std::cout << "patch 2 finished" << std::endl;
 
     std::cout << "finished patches returning to debug event" << std::endl;
+
+    return true;
 }
 
 int main(int argc, char** argv) {
@@ -202,7 +328,7 @@ int main(int argc, char** argv) {
     PROCESS_INFORMATION pi                          {};
     char                full_image_path[MAX_PATH]   {};
 
-	if (argc <= 1 || argc >= 3)
+	if (argc != 2)
 	{
 		print_usage(); 
 		return 1;
@@ -221,7 +347,7 @@ int main(int argc, char** argv) {
         std::cout << "process creation failed: " << GetLastError() << std::endl;
         return 3;
     }
-    std::cout << "created process:  " << std::dec << pi.dwProcessId << std::endl;
+    std::cout << "created process: " << std::dec << pi.dwProcessId << std::endl;
 
     if (!wait_for_module_load(pi, L"UnityPlayer.dll", on_unityplayer_load)) {
         std::cout << "failed to wait for module load: " << GetLastError() << std::endl;
